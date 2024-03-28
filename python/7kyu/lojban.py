4aaf7112ec542c028ca190820760fc5d624992db
TheGoldKay <jhonnatanwfc@live.com>
2024-03-28 15:23:16 -0300

    more pythonic

--- python/7kyu/lojban.py
+++ python/7kyu/lojban.py
@@ -1,24 +1,3 @@
-loj = dict()
-loj['pa'] = 1
-loj['re'] = 2
-loj['ci'] = 3
-loj['vo'] = 4
-loj['mu'] = 5
-loj['xa'] = 6
-loj['ze'] = 7
-loj['bi'] = 8 
-loj['so'] = 9
-loj['no'] = 0
-        
 def convert_lojban(lojban):
-    num = loj.keys()
-    ban = list(lojban) 
-    word = ''
-    res = ''
-    while ban:
-        word += ban[0]
-        if word in num:
-            res += str(loj[word])
-            word = ''
-        del ban[0]
-    return int(res)
+    seq = ['no', 'pa', 're', 'ci', 'vo', 'mu', 'xa', 'ze', 'bi', 'so']
+    return int(''.join([str(seq.index(lojban[i: i+2])) for i in range(0, len(lojban), 2)]))
